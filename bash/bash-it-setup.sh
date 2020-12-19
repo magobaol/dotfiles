04df8dbfd6e75a5bee445fba5de58bdb745e3110
Francesco Face <francesco.face@gmail.com>
2020-12-19 08:57:19 +0100

    Move bash-it setup commands into a separate file, to make dotbot install script shell agnostic

---
+++ bash/bash-it-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ~/.bash/bash-it.bash && bash-it enable completion git bash-it system ssh
+source ~/.bash/bash-it.bash && bash-it enable alias general
+source ~/.bash/bash-it.bash && bash-it enable plugin base alias-completion battery
