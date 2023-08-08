f94e0d7265a3e67b199b0460eea1563a3f69432a
Francesco Face <francesco.face@gmail.com>
2023-08-08 19:38:31 +0200

    Evolve to_zsh and to_bash aliases consider the current architecture

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,6 +1,26 @@
-alias to_zsh='chsh -s /usr/local/bin/zsh'
-alias to_bash='chsh -s /usr/local/bin/bash'
 alias ip='ifconfig | grep inet'
 alias punf='php bin/phpunit --filter '
 alias sf='bin/console'
 alias pun='php bin/phpunit'
+
+to_zsh()
+{
+  # Apple Silicon
+  if [[ $(uname -p) == 'arm' ]]; then
+    chsh -s /opt/homebrew/bin/zsh
+  else
+    # Intel
+    chsh -s /usr/local/bin/zsh
+  fi
+}
+
+to_bash()
+{
+  # Apple Silicon
+  if [[ $(uname -p) == 'arm' ]]; then
+    chsh -s /opt/homebrew/bin/bash
+  else
+    # Intel
+    chsh -s /usr/local/bin/bash
+  fi
+}
