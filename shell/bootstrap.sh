9a07e5ad20e952593c4abf34565d002aa7832e40
Francesco Face <francesco.face@gmail.com>
2023-11-24 20:03:07 +0100

    Add shell configuration for nvm

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -2,3 +2,8 @@ PATH=~/.dotfiles/bin:/usr/local/bin:${PATH}
 
 export GOPATH="${HOME}/go"
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
