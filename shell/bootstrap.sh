c8bbd4097669721b53766e35865a2f52158a93ff
Francesco Face <francesco.face@gmail.com>
2025-07-31 12:52:40 +0200

    Force english as language in the terminal

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -2,6 +2,7 @@ PATH=~/.dotfiles/bin:/usr/local/bin:${PATH}
 
 export GOPATH="${HOME}/go"
 export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+export LANG=en_US.UTF-8
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
